37441593d2d196be27ec60e9bcda6e13faa2a3a6
Loay Al-Said <l.programming555@gmail.com>
2024-12-18 23:29:46 +0200

    RenderCount

--- src/useRef/Main.jsx
+++ src/useRef/Main.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
 export default function Main() {
 	const [name, setName] = useState('');
-	
+	const renderCount = useRef(0);
+
+	useEffect(() => {
+		renderCount.current += 1;
+	});
+
 	return <>
 		<input type="text" value={name} onChange={(event) => setName(event.target.value)} />
 		<div> My name is: {name}</div>
+		<div>I rendered { renderCount.current}</div>
 	</>
 }
