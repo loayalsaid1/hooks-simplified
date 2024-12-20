3f7574219450e334dc33cef8849c9a653fb7e9aa
Loay Al-Said <l.programming555@gmail.com>
2024-12-21 00:00:11 +0200

    useTransition

--- src/useTransition/Main.js
+++ src/useTransition/Main.js
@@ -1,23 +1,31 @@
 import React, { useState, useTransition } from 'react'
 
+
 export default function Main() {
+	const [isPending, startTransition] = useTransition();
 	const [text, setText] = useState('');
 	const [list, setList] = useState([]);
 
+
 	const handleChange = (e) => {
 		setText(e.target.value);
-		const temp = [];
-		for ( let i = 0; i < 20000; i++) {
+		
+		startTransition(() => {
+			const temp = [];
+			for ( let i = 0; i <15000; i++) {
 			temp.push(e.target.value);
 		}
 		setList(temp);
+		})
 	}
 
 	return (
 		<>
 		<input type='text' value={text} onChange={handleChange} />
 		{
-			list.map((item, index) => <div key={index}>{item}</div>)
+			isPending
+			? 'Loading ... '
+			: list.map((item, index) => <div key={index}>{item}</div>)
 		}
 		</>
 	)
