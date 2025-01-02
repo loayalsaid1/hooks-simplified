d46c086b5b4a4601afd43be8e349b205bf9c3877
Loay Al-Said <l.programming555@gmail.com>
2025-01-02 16:28:28 +0200

    Use useId with 2 elements per compoennt

--- src/useId/EmailForm.jsx
+++ src/useId/EmailForm.jsx
@@ -3,11 +3,15 @@ import React, { useId } from 'react'
 export default function EmailForm() {
 	// if to elemetns rendered in teh screen.. this means.. same Id will be used more that once..!
 	const id = useId();
+	const id2 = useId();
 
 	return (
 		<>
 		<label htmlFor={id}>Email: </label>
 		<input id={id} type='text' />
+		<br />
+		<label htmlFor={id2}>Name: </label>
+		<input id={id2} type='text' />
 		</>
 	)
 }
