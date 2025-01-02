402ee9412a1ae51139bdc4ad51dc51566ef514b5
Loay Al-Said <l.programming555@gmail.com>
2025-01-02 18:41:16 +0200

    Use the useLocalStorage custom hook

--- src/customHooks/useLocalStorage/useLocalStorage.hook.js
+++ src/customHooks/useLocalStorage/useLocalStorage.hook.js
@@ -1,8 +1,24 @@
 import { useEffect, useState} from 'react';
 
 
-export default function useLocalStorage(initialValue) {
-	const [value, setValue] = useState(initialValue);
+function getSavedValue(key, initialValue) {
+	const value = localStorage.getItem(key);
+	if (value) return JSON.parse(value);
 	
+	if (initialValue instanceof Function) return initialValue();
+	return initialValue;
+}
+
+export default function useLocalStorage(key, initialValue) {
+	const [value, setValue] = useState(() => {
+		return getSavedValue(key, initialValue);
+	});
+
+	
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [value]);
+
+
 	return [value, setValue];
 }
