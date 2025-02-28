8a40986119d3d886a61dfab1c9166aaee57b149c
Loay Al-Said <l.programming555@gmail.com>
2025-02-28 23:57:33 +0200

    Add the hook video

--- src/HookVideo.jsx
+++ src/HookVideo.jsx
@@ -2,15 +2,24 @@ import React, { useContext } from 'react';
 import { hookContext } from './MainHooksComponent';
 
 export default function HookVideo() {
-  const { hookName } = useContext(hookContext);
+  const { hookName, videoLink } = useContext(hookContext);
 
   return (
     <section>
-      <h5  className='subtitle'>📺 Hook Vidoe</h5>
+      <h5 className="subtitle">📺 Hook Vidoe</h5>
       <p>
-        Here should go either the link to teh video tutorial for{' '}
-        <strong>{hookName}</strong> or the video itself
+        Here is a tutorial explaining <strong>{hookName}</strong>
       </p>
+      <small>Again... WRITE THE CODE YOURSELF.. Bit by bit untill it's reasonable at your mind... not at one session even..</small>
+      <iframe
+        className='hook-video'
+        src={`https://www.youtube.com/embed/${new URL(videoLink).searchParams.get('v')}`}
+        title={hookName}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </section>
   );
 }
